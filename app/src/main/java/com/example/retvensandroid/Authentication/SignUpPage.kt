4849d5f39b6923e5cd845a369419105c798a6df2
rahul1478 <65287732+rahul1478@users.noreply.github.com>
2023-02-02 10:36:14 +0530

    Project Created

---
+++ app/src/main/java/com/example/retvensandroid/Authentication/SignUpPage.kt
@@ -0,0 +1,12 @@
+package com.example.retvensandroid.Authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.retvensandroid.R
+
+class SignUpPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_page)
+    }
+}
